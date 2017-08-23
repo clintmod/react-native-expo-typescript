0441a29beee1f0ccebc8f4688b8396e8bd96bed1
Clint Modien <cmodien@gmail.com>
2017-08-23 12:38:54 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,3 @@
+import App from './build/dist/index';
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
+ 
+interface Props {
+}
+ 
+interface State {
+    
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  } as ViewStyle,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  } as TextStyle,
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  } as TextStyle,
+});
+ 
+export default class App extends React.Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          {'To get started, edit src/index.tsx'}
+        </Text>
+      </View>
+    );
+  }
+}
